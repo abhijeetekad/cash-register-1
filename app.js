e65f7520abed70770fe72be5dece74d08e53f180
nisargpawade <70917826+nisargpawade@users.noreply.github.com>
2021-08-25 20:55:35 +0530

    added styles.css and app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const billAmount = document.querySelector('#bill-amount');
+const nextBtn = document.querySelector('#next-btn');
+const cashGiven = document.querySelector('#cash-given');
+const checkBtn = document.querySelector('#check-btn');
+const errorMsg = document.querySelector('#error-messagge');
+
+checkBtn.addEventListener("click", function clickHandler() {
+    console.log("clicked on check")
+})
