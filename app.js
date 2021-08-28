f4fbea149446232388d89ecf1e08d028f9a787a0
nisargpawade <70917826+nisargpawade@users.noreply.github.com>
2021-08-28 16:13:41 +0530

    function to validate amount

--- app.js
+++ app.js
@@ -2,8 +2,35 @@ const billAmount = document.querySelector('#bill-amount');
 const nextBtn = document.querySelector('#next-btn');
 const cashGiven = document.querySelector('#cash-given');
 const checkBtn = document.querySelector('#check-btn');
-const errorMsg = document.querySelector('#error-messagge');
+const errorMsg = document.querySelector('#error-message');
 
-checkBtn.addEventListener("click", function clickHandler() {
-    console.log("clicked on check")
-})
+const notes = [2000, 500, 100, 20, 10, 5, 1]
+
+// function calculateChange(amtToReturn) {
+//     for (i, i< notes.length, i++) {}
+// };
+
+function showMsg(msg) {
+    errorMsg.innerText = msg;
+    errorMsg.style.display = "block";
+};
+
+function hideMsg() {
+    errorMsg.style.display = "none";
+};
+
+nextBtn.addEventListener("click", function validateBillAmt() {})
+
+checkBtn.addEventListener("click", function validateAmt() {
+    hideMsg();
+    if (billAmount.value > 0) {
+        if (cashGiven.value >= billAmount.value) {
+            showMsg("Approved");
+            const amtToReturn = cashGiven.value - billAmount.value;
+            console.log(amtToReturn)
+        } else {
+            showMsg("Cash given is less than the Bill Amount")};
+    }
+    else {
+        showMsg("Enter valid Bill Amount")};
+});
